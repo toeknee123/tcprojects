0b0f68e3af4ced7fbcc3c4d745788c3c35856e5f
Tony Chan <tonychan@tonys-mbp.attlocal.net>
2016-10-14 17:31:53 -0500

    added comment

--- hungrygames/jsfile.js
+++ hungrygames/jsfile.js
@@ -13,6 +13,7 @@ var restaurantNames = [
 ];
 
 //function used to populate answer after button is clicked
+//var rand is added on the second line to allow refresh when button is clicked. 
 function myRestaurant () {
 	var rand = restaurantNames[Math.floor(Math.random() * restaurantNames.length)];
 	document.getElementById("restaurant").innerHTML = rand;
