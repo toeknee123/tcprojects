231deb3ed47eea346d2c92729e169acdc4a4a1a6
Tony Chan <tonychan@tonys-mbp.attlocal.net>
2016-10-14 14:32:44 -0500

    starting new project

---
+++ hungrygames/jsfile.js
@@ -0,0 +1,12 @@
+function myFunction () {
+	document.getElementById("restaurant").innerHTML = "We should go to....";
+}
+
+function mathstuff () {
+	var x = 5
+	var y = 6
+	var z = x + y;
+	document.getElementById("math").innerHTML = z;
+}
+
+
